aed1c1ced4be6acefdcae66a3bace14797f6b021
Shadya Gamal <shadya.gamal@epfl.ch>
2020-10-10 13:10:24 +0200

    Amélioration 2 de computer_player.py tentatives avec le nombre exactement entre min et max

--- computer_player.py
+++ computer_player.py
@@ -3,10 +3,10 @@
 '''
 this script will try to guess the mysterious number
 prepared by guess_my_number.GuessMachine.
-It's strategy is to try random numbers.
+It's strategy is to try middle number between min and max.
+It'll adapt min and max values to match GuessMachine answers.
 '''
 
-import random
 from guess_my_number import MIN, MAX, GuessMachine
 
 if __name__=='__main__':
@@ -14,13 +14,13 @@ if __name__=='__main__':
     max= MAX
     guess_machine=GuessMachine()
     while True:
-        attempt= random.randint(MIN,MAX)
+        attempt= int((min+max)/2)
         result= guess_machine.guess(attempt)
         print('tried %d : %s' % (attempt, result))
         if result == 'found':
             print('Finished in %d attempts.' % guess_machine.number_of_attempt)
             break
         elif result == 'too low':
-            min=attempt +1
+            min = attempt +1
         else:
             max = attempt -1
